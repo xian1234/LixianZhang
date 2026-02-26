d8779410d68bfae9861ef15b278ed2af527a9752
Xian <767374704@qq.com>
2026-02-26 12:11:42 +0800

    Update PublicationsCn.tsx

--- src/cn/sections/PublicationsCn.tsx
+++ src/cn/sections/PublicationsCn.tsx
@@ -3,32 +3,53 @@ import { BookOpen, ExternalLink } from 'lucide-react';
 const PublicationsCn = () => {
   const publications = [
     {
+      title: 'A2-MAE: A spatial-temporal-spectral unified remote sensing pre-training method based on anchor-aware masked autoencoder',
+      authors: 'Zhang, L., Zhao, Y., Dong, R., Zhang, J., Yuan, S., Cao, S., ... & Fu, H.',
+      venue: 'IEEE Transactions on Geoscience and Remote Sensing, 2026',
+      badges: ['Q1', 'IF=8.6'],
+      link: 'https://ieeexplore.ieee.org/document/11396052'
+    },
+    {
+      title: 'Swcare: Switchable learning and connectivity-aware refinement method for multi-city and diverse-scenario road mapping using remote sensing images',
+      authors: 'Zhang, L., Yuan, S., Dong, R., Zheng, J., Gan, B., Fang, D., ... & Fu, H.',
+      venue: 'International Journal of Applied Earth Observation and Geoinformation, 127: 103665, 2024',
+      badges: ['Q1', 'IF=8.6'],
+      link: 'https://www.sciencedirect.com/science/article/pii/S1569843224000190'
+    },
+    {
       title: 'A prolonged artificial nighttime-light dataset of China (1984-2020)',
       authors: 'Zhang, L., Ren, Z., Chen, B., Gong, P., Xu, B., & Fu, H.',
-      venue: 'Scientific Data, 2024',
-      badges: ['JCR Q1', 'IF=9.8', 'ESI 高被引'],
-      link: 'https://www.nature.com/articles/s41597-024-03223-1',
+      venue: 'Scientific Data, 11(1), 414, 2024',
+      badges: ['Q1', 'IF=6.9', 'ESI highly cited'],
+      link: 'https://www.nature.com/articles/s41597-024-03223-1'
     },
     {
       title: 'DeepLight: Reconstructing High-Resolution Observations of Nighttime Light With Multi-Modal Remote Sensing Data',
       authors: 'Zhang, L., Dong, R., Yuan, S., Zhang, J., Chen, M., Zheng, J., & Fu, H.',
-      venue: 'IJCAI 2024',
+      venue: 'IJCAI 2024, 7563-7571',
       badges: ['CCF-A'],
-      link: 'https://dl.acm.org/doi/10.24963/ijcai.2024/837',
+      link: 'https://dl.acm.org/doi/10.24963/ijcai.2024/837'
     },
     {
-      title: 'Swcare: Switchable learning and connectivity-aware refinement method for road mapping',
-      authors: 'Zhang, L., Yuan, S., Dong, R., Zheng, J., Gan, B., Fang, D., ... & Fu, H.',
-      venue: 'JAG, 2024',
-      badges: ['JCR Q1', 'IF=7.5'],
-      link: 'https://www.sciencedirect.com/science/article/pii/S1569843224000190',
+      title: 'Making low-resolution satellite images reborn: a deep learning approach for super-resolution building extraction',
+      authors: 'Zhang, L., Dong, R., Yuan, S., Li, W., Zheng, J., & Fu, H.',
+      venue: 'Remote Sensing, 13(15), 2021',
+      badges: ['Q2', 'IF=5.0'],
+      link: 'https://www.mdpi.com/2072-4292/13/15/2872'
     },
     {
-      title: 'A2-MAE: A spatial-temporal-spectral unified remote sensing pre-training method',
-      authors: 'Zhang, L., Zhao, Y., Dong, R., Zhang, J., Yuan, S., Cao, S., ... & Fu, H.',
-      venue: 'IEEE TGRS, 2026',
-      badges: ['JCR Q1', 'IF=8.3'],
-      link: 'https://ieeexplore.ieee.org/document/11396052',
+      title: 'Building Bridges across Spatial and Temporal Resolutions: Reference-Based Super-Resolution via Change Priors and Conditional Diffusion Model',
+      authors: 'Dong, R., Yuan, S., Luo, B., Chen, M., Zhang, J., Zhang, L.*, & Fu, H.*',
+      venue: 'CVPR 2024: 27684-27694',
+      badges: ['CCF-A'],
+      link: '#'
+    },
+    {
+      title: 'RRSGAN: Reference-based super-resolution for remote sensing image',
+      authors: 'Dong, R., Zhang, L., & Fu, H.',
+      venue: 'IEEE Transactions on Geoscience and Remote Sensing, 60: 1-17, 2021',
+      badges: ['Q1', 'IF=8.6', 'ESI highly cited'],
+      link: 'https://ieeexplore.ieee.org/document/9328132'
     },
   ];
 
